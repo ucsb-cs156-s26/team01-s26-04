e2bd4b89a0ce086dc0a2b10fa3bf7755ec3b58f2
ertanbasgul <meb@ucsb.edu>
2026-04-24 16:59:59 -0700

    adding RecommendationRequestController BUT MAY NOT WORK WITHOUT MY ENTITY AND REPOSITORY FILES

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -1,17 +1,23 @@
 package edu.ucsb.cs156.example.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/** This is a REST controller for UCSBDates */
+/** This is a REST controller for RecommendationRequest */
 @Tag(name = "RecommendationRequest")
 @RequestMapping("/api/recommendationrequest")
 @RestController
@@ -21,15 +27,61 @@ public class RecommendationRequestController extends ApiController {
   @Autowired RecommendationRequestRepository recommendationRequestRepository;
 
   /**
-   * List all UCSB dates
+   * List all RecommendationRequest
    *
-   * @return an iterable of UCSBDate
+   * @return an iterable of RecommendationRequest
    */
-  @Operation(summary = "initial")
+  @Operation(summary = "List all recommendationrequests")
   @PreAuthorize("hasRole('ROLE_USER')")
   @GetMapping("/all")
   public Iterable<RecommendationRequest> allRecommendationRequest() {
-    Iterable<RecommendationRequest> obj = recommendationRequestRepository.findAll();
-    return obj;
+    Iterable<RecommendationRequest> recommendationrequests =
+        recommendationRequestRepository.findAll();
+    return recommendationrequests;
+  }
+
+  /**
+   * Create a new recommendationrequest
+   *
+   * @param requesterEmail
+   * @param professorEmail
+   * @param explanation
+   * @param dateRequested
+   * @param dateNeeded
+   * @param done
+   * @return the saved recommendationrequest
+   */
+  @Operation(summary = "Create a new date")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @PostMapping("/post")
+  public RecommendationRequest postRecommendationRequest(
+      @Parameter(name = "requesterEmail") @RequestParam String requesterEmail,
+      @Parameter(name = "professorEmail") @RequestParam String professorEmail,
+      @Parameter(name = "explanation") @RequestParam String explanation,
+      @Parameter(name = "dateRequested") @RequestParam String dateRequested,
+      @Parameter(
+              name = "dateRequested",
+              description =
+                  "date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)")
+          @RequestParam("dateRequested")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+          LocalDateTime localDateTime)
+      throws JsonProcessingException {
+
+    // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    // See: https://www.baeldung.com/spring-date-parameters
+
+    log.info("dateRequested={}", dateRequested);
+
+    RecommendationRequest recommendationRequest = new RecommendationRequest();
+    recommendationRequest.setRequesterEmail(requesterEmail);
+    recommendationRequest.setProfessorEmail(professorEmail);
+    recommendationRequest.setExplanation(explanation);
+    recommendationRequest.setDateRequested(localDateTime);
+
+    RecommendationRequest savedRecommendationRequest =
+        recommendationRequestRepository.save(recommendationRequest);
+
+    return savedRecommendationRequest;
   }
 }
