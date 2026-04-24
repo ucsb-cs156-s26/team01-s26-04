2d4101c1a84a12c3dc56e108ee540099ba05b041
Vanessa <vanessaxu@ucsb.edu>
2026-04-24 11:53:29 -0700

    Add UCSBDiningCommonsMenuItemController

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
@@ -0,0 +1,64 @@
+package edu.ucsb.cs156.example.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.UCSBDiningCommonsMenuItemRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "UCSBDiningCommonsMenuItem")
+@RequestMapping("/api/ucsbdiningcommonsmenuitem")
+@RestController
+@Slf4j
+public class UCSBDiningCommonsMenuItemController {
+  @Autowired UCSBDiningCommonsMenuItemRepository ucsbDiningMenuItems;
+
+  @Operation(summary = "List all ucsb dining hall menu items")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("/all")
+  public Iterable<UCSBDiningCommonsMenuItem> allUCSBDiningHallMenuItems() {
+    Iterable<UCSBDiningCommonsMenuItem> menuItem = ucsbDiningMenuItems.findAll();
+    return menuItem;
+  }
+
+  @Operation(summary = "Get a single dining hall menu item")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("")
+  public UCSBDiningCommonsMenuItem getById(@Parameter(name = "id") @RequestParam Long id) {
+    UCSBDiningCommonsMenuItem ucsbMenuItem =
+        ucsbDiningMenuItems
+            .findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommonsMenuItem.class, id));
+
+    return ucsbMenuItem;
+  }
+
+  @Operation(summary = "Create a new dining hall menu item")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @PostMapping("/post")
+  public UCSBDiningCommonsMenuItem postUCSBDiningHallMenuItems(
+      @Parameter(name = "diningCommonsCode") @RequestParam String diningCommonsCode,
+      @Parameter(name = "name") @RequestParam String name,
+      @Parameter(name = "station") @RequestParam String station)
+      throws JsonProcessingException {
+
+    UCSBDiningCommonsMenuItem item = new UCSBDiningCommonsMenuItem();
+    item.setDiningCommonsCode(diningCommonsCode);
+    item.setName(name);
+    item.setStation(station);
+
+    UCSBDiningCommonsMenuItem savedMenuItem = ucsbDiningMenuItems.save(item);
+
+    return savedMenuItem;
+  }
+}
