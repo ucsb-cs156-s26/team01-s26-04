e8fdce53d6e9f103d57df3aa16305f91bf12e8fd
Vincent Nguyen <vincent150@ucsb.edu>
2026-04-27 21:12:45 -0700

    articles -put

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -7,7 +7,6 @@ import edu.ucsb.cs156.example.repositories.ArticlesRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +15,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -100,35 +97,6 @@ public class ArticlesController extends ApiController {
     Articles savedArticle = articlesRepository.save(article);
 
     return savedArticle;
-  }
-
-  /**
-   * Update a single article
-   *
-   * @param id id of the article to update
-   * @param incoming the new article
-   * @return the updated article object
-   */
-  @Operation(summary = "Update a single article")
-  @PreAuthorize("hasRole('ROLE_ADMIN')")
-  @PutMapping("")
-  public Articles updateArticle(
-      @Parameter(name = "id") @RequestParam Long id, @RequestBody @Valid Articles incoming) {
-
-    Articles articles =
-        articlesRepository
-            .findById(id)
-            .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
-
-    articles.setTitle(incoming.getTitle());
-    articles.setUrl(incoming.getUrl());
-    articles.setExplanation(incoming.getExplanation());
-    articles.setEmail(incoming.getEmail());
-    articles.setDateAdded(incoming.getDateAdded());
-
-    articlesRepository.save(articles);
-
-    return articles;
   }
 
   /**
